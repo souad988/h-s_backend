d5a042c89f74f9e29788e7393b5d45065538cbce
souad988 <souadelmansouri88@gmail.com>
2023-11-27 17:43:03 +0100

    Add role and confirmed to serialized attributes of user

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -9,7 +9,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
       redirect_to "http://localhost:3000/login" # Modify this URL to your frontend confirmation success page
     else
       # Handle resource errors or invalid token
-      redirect_to "http://localhost:3000/confirmation_error/id=#{params[:email]}" # Modify this URL to your frontend confirmation error page
+      redirect_to "http://localhost:3000/confirmation_error/#{params[:email]}" # Modify this URL to your frontend confirmation error page
     end
   end
 
@@ -18,9 +18,17 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
     if resource && !resource.confirmed?
       resource.send_confirmation_instructions
-      redirect_to "#{ENV['FRONTEND_URL']}/confirmation_sent" # Modify this URL to your frontend confirmation sent page
+      render json: {
+        status: {code: 200, message: 'Confirmation sent successfully!!.'},
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+      #redirect_to "#{ENV['FRONTEND_URL']}/confirmation_sent" # Modify this URL to your frontend confirmation sent page
     else
-      redirect_to "#{ENV['FRONTEND_URL']}/already_confirmed" # Modify this URL to your frontend already confirmed page
+      render json: {
+        status: {code: 400, message: 'Email already confirmed.!'},
+        data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+      #redirect_to "#{ENV['FRONTEND_URL']}/already_confirmed" # Modify this URL to your frontend already confirmed page
     end
   end
 end
