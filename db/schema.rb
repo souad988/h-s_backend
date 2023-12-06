e60598dc641631f9220decbeeb6abd9868b37008
souad988 <souadelmansouri88@gmail.com>
2023-12-06 17:59:50 +0100

    Generate categories and add parent reference to create subcategories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_27_143636) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_06_165648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_143636) do
     t.string "zipcode"
     t.string "number"
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "parent_id"
+    t.index ["parent_id"], name: "index_categories_on_parent_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -54,4 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_143636) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "categories", "categories", column: "parent_id"
 end
