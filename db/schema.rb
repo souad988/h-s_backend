18ad2bbb9b98830454b8441c5371d36403bd1dd8
Souad El Mansouri <souadelmansouri88@gmail.com>
2024-02-02 08:35:48 +0100

    Add UserAdress model to represent many to many relation between user and adress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_27_143636) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_02_072608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,18 +18,25 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_143636) do
     t.string "street"
     t.string "city"
     t.string "country"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "zipcode"
     t.string "number"
-    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_addresses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "address_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_user_addresses_on_address_id"
+    t.index ["user_id"], name: "index_user_addresses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,5 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_27_143636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "addresses", "users"
+  add_foreign_key "user_addresses", "addresses"
+  add_foreign_key "user_addresses", "users"
 end
